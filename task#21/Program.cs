bd3ad67a67002c15a7f09a27f18d7b87f1c65f3e
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-09 23:36:52 +0300

    Задача № 21

--- task#21/Program.cs
+++ task#21/Program.cs
@@ -4,11 +4,10 @@
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 
-AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)*2*/
+AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)*2
+*/
 
-void GetDistanceBetweenThreePoints(int firstX, int secondX, 
-                                    int firstY, int secondY,
-                                    int firstZ, int secondZ)
+void GetDistanceBetweenThreePoints(int firstX, int secondX, int firstY, int secondY, int firstZ, int secondZ)
 {
     double result = 0;
     double TempX = Math.Pow(secondX - firstX, 2); 
@@ -16,7 +15,7 @@ void GetDistanceBetweenThreePoints(int firstX, int secondX,
     double TempZ = Math.Pow(secondZ - firstZ, 2);
     result = Math.Sqrt(TempX + TempY + TempZ);
     Console.WriteLine();
-    Console.WriteLine($"Расстояние между двумя точками {result}");
+    Console.WriteLine($"Расстояние между двумя точками в 3D пространстве {result}");
     Console.WriteLine();    
 }
 GetDistanceBetweenThreePoints(3, 2, 6, 1, 8, -7);
