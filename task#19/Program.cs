ced7b795eb10b1dc966b50f5635032f50304a7ba
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-10 12:56:05 +0300

    Задача 19

--- task#19/Program.cs
+++ task#19/Program.cs
@@ -11,14 +11,12 @@
     Console.WriteLine();
     {
     if(number < 100000 && number > 9999)
-    {
         Console.WriteLine($"Введенное число {number} --->");
-    }
     else
     {
         Console.WriteLine("Введено не пятизначное число. Попробуйте еще раз))");
-        return;}
-    }       
+        return;
+    }   
     string numberAsString = number.ToString();
     int indexNULL = int.Parse(numberAsString[0].ToString());
     int indexFIRST = int.Parse(numberAsString[1].ToString());
@@ -31,5 +29,6 @@
     {
         Console.WriteLine("НЕ является ПАЛИНДРОМОМ, так как индексы NULL THIRD не равны и индексы FIRST FORTH не равны также.");
         return;
+    }
     }
     
